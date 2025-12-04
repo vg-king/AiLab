6ef48ef200cc8342302b38fa3c13e5366b437aaf
vg-king <vg3772285@gmail.com>
2025-12-04 10:03:56 +0530

    reviewFix

--- Gata.java
+++ Gata.java
@@ -3,18 +3,21 @@ import java.util.Scanner;
 public class Gata {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Choose Gate :");
+        System.out.println("Choose Gate (AND, OR, NOT, XOR, NOR, NAND):");
         String gate = sc.next().toUpperCase();
         int a = 0,b=0;
         if (!gate.equals("NOT")) {
-            System.out.println("Enter first input (0 or 1)");
-            a=sc.nextInt();
-            System.out.println("ENter second input (0 or 1)");
+            System.out.println("Enter first input (0 or 1):");
+            a = sc.nextInt();
+            System.out.println("Enter second input (0 or 1):");
             b = sc.nextInt();
+            a = (a == 0) ? 0 : 1;
+            b = (b == 0) ? 0 : 1;
         }
         else{
-            System.out.println("Enter input (0 or 1): ");
+            System.out.println("Enter input (0 or 1):");
             a = sc.nextInt();
+            a = (a == 0) ? 0 : 1;
         }
         int result = 0;
         switch (gate) {
@@ -35,7 +38,12 @@ public class Gata {
                 break;
             
             case "NOR":
-                result = (a|b)==1?0:1;
+                
+                result = 1 - (a | b);
+                break;
+
+            case "NAND":
+                result = 1 - (a & b);
                 break;
             
             default:
